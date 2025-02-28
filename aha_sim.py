5227f15af29de574443494da5323c5b7a89bf6f0
Tom Thuma <tomthuma@Toms-MacBook-Air.local>
2025-02-28 00:26:27 -0600

    actual initail commit

--- aha_sim.py
+++ aha_sim.py
@@ -1,3 +1,18 @@
 class team:
     def __init__(self, points):
         self.points = points
+
+    def game(self, result):
+        outcomes = ['win', 'loss', 'tie', 'ot_loss']
+        if result not in outcomes:
+            print('result not a possible outcome')
+            exit()
+        
+        if result == 'win':
+            self.points += 2
+        elif result == 'tie' or result == 'ot_loss':
+            self.points += 1
+    
+class sim:
+    def __init__(self, teams):
+        self.teams = teams
